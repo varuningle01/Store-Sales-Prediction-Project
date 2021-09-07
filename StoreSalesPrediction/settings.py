507594d046c08a7ed6bbfc74184df6bca6920c7f
Varun Ingle <72153612+scorpion-varun@users.noreply.github.com>
2021-09-07 09:27:38 +0530

    Update settings.py

--- StoreSalesPrediction/settings.py
+++ StoreSalesPrediction/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^ath=akwz%_9x9e^rp(^ufs2au5427!+6#$%4cv1vgrl(j0x*p'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
